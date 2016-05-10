aecb025ea429622d02b0071b16f3f8af5af2fd03
Samuel Gillet <samuelgillet@hotmail.com>
2016-05-09 20:50:44 -0600

    missing semicolon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
 	console.log('Express server listening on port ' + port);
 });
