e6f01394b58c22a3924fb6d54380c321b57ab72e
Samuel Gillet <samuelgillet@hotmail.com>
2016-05-09 20:34:59 -0600

    basic v1 app is done

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
-var server = app.listen(7777, function() {
-	console.log('Express server listening on port ' + server.address().port);
+var port = process.env.PORT || 3000
+var server = app.listen(port, function() {
+	console.log('Express server listening on port ' + port);
 });
--- public/main.js
+++ public/main.js
@@ -9,12 +9,37 @@ var stateNames = ["Hawaii","Alaska","Florida","South_Carolina","Georgia",
 "Indiana","Illinois","Minnesota","Wisconsin","Missouri",
 "Arkansas","Oklahoma","Kansas","Louisiana","Virginia"]
 
+var incrementor = 0;
+
 $(document).on('ready', function(){
   $(document).on('submit', '.input-text', function(e){
     e.preventDefault();
-    if($('.input1').val()=="Alaska"){
-      $('#'+'Alaska').attr("class", "color green")
-      $('.input1').val(",")
-    }
+    var inputVal = $('.input1').val().replace(' ','_').trim();
+    if ($.inArray(inputVal, stateNames) == -1) {
+      $('.input1').val("")
+      bootbox.alert("Excuse me but Homey most certainly does NOT play that! Ahem... Please remeber to capitalize state names!!", function(){
+        console.log("Alert Callback Error")
+      })
+    } else if ($.inArray(inputVal, stateNames) >= 0) {
+        $('#'+inputVal).attr("class", "color green");
+        $('.input1').val("")
+        ++incrementor;
+        $('.stats').text("States Named: " + incrementor)
+        if (incrementor == 10) {
+          bootbox.alert("WOW! You got 10 states already? You so smart!... keep it up!", function(){
+            console.log("Alert Callback 10 states")
+          })
+        }
+        if (incrementor == 25) {
+          bootbox.alert("Whooaahh you're halfway there!!! Whooaahhh livin on a ....", function(){
+            console.log("Alert Callback 25 states")
+          })
+        }
+        if (incrementor == 50) {
+          bootbox.alert("CONGRATULATIONS!!! You won this amazing game! Hit refresh to play again", function(){
+            console.log("Alert Callback Winner")
+          })
+        }
+      }
   })
 });
