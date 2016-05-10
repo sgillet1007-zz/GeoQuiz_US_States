13ae771d0bd02bf9775534038c507ffe51bdeea1
Samuel Gillet <samuelgillet@hotmail.com>
2016-05-09 21:16:27 -0600

    global var order

--- public/main.js
+++ public/main.js
@@ -1,17 +1,17 @@
-var stateNames = ["Hawaii","Alaska","Florida","South_Carolina","Georgia",
-"Alabama","North_Carolina","Tennessee","Rhode_Island","Connecticut",
-"Massachusetts","Maine","New_Hampshire","Vermont","New_York",
-"New_Jersey","Pennsylvania","Delaware","Maryland","West_Virginia",
-"Kentucky","Ohio","Michigan","Wyoming","Montana",
-"Idaho","Washington","Texas","California","Arizona",
-"Nevada","Utah","Colorado","New_Mexico","Oregon",
-"North_Dakota","South_Dakota","Nebraska","Iowa","Mississippi",
-"Indiana","Illinois","Minnesota","Wisconsin","Missouri",
-"Arkansas","Oklahoma","Kansas","Louisiana","Virginia"]
-
-var incrementor = 0;
-
 $(document).on('ready', function(){
+  var stateNames = ["Hawaii","Alaska","Florida","South_Carolina","Georgia",
+  "Alabama","North_Carolina","Tennessee","Rhode_Island","Connecticut",
+  "Massachusetts","Maine","New_Hampshire","Vermont","New_York",
+  "New_Jersey","Pennsylvania","Delaware","Maryland","West_Virginia",
+  "Kentucky","Ohio","Michigan","Wyoming","Montana",
+  "Idaho","Washington","Texas","California","Arizona",
+  "Nevada","Utah","Colorado","New_Mexico","Oregon",
+  "North_Dakota","South_Dakota","Nebraska","Iowa","Mississippi",
+  "Indiana","Illinois","Minnesota","Wisconsin","Missouri",
+  "Arkansas","Oklahoma","Kansas","Louisiana","Virginia"]
+
+  var incrementor = 0;
+  
   $(document).on('submit', '.input-text', function(e){
     e.preventDefault();
     var inputVal = $('.input1').val().replace(' ','_').trim();
